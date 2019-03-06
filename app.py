9be42dc75d93b67f16a66cd40ffa53921c3aeaba
manuelhelaman <manuel.helaman.soto@gmail.com>
2019-03-06 10:23:33 -0700

    added Procfile

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_heroku import Heroku
 
 app = Flask(__name__)
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+heroku = Heroku(app)
+db = SQLAlchemy(app)
+
+class Books(db.Model):
+    __tablename__ = "books"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    author = db.Column(db.String(80))
+
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def __repr__(self):
+        return '<Title %r>' % self.title        
 
 @app.route('/')
 def home():
